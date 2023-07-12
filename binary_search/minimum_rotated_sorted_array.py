afc67ed20259168aac6b71520911662c686349c8
TejasAnand <anandtejas455@gmail.com>
2023-07-12 21:22:56 +0530

    file struc

---
+++ binary_search/minimum_rotated_sorted_array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        minimum=nums[0]
+
+        #implementing binary search here 
+
+        lo = 0
+        hi = len(nums)-1
+
+        while lo<=hi:
+            mid = (lo+hi)//2
+
+            if nums[lo]<nums[hi]:
+                minimum= min(minimum,nums[lo])
+                break
+            minimum = min(minimum,nums[mid])
+            if nums[mid]>=nums[lo]:
+                lo = mid+1  #searching right
+            else:
+                hi = mid-1  #searching left
+        return minimum
