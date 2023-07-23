daa8f82b4db2368f986b7b4ddbf23d35cead7a1a
TejasAnand <anandtejas455@gmail.com>
2023-07-23 18:25:36 +0530

    insertion sort algo template

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -1,12 +1,18 @@
-# Python implementation of Insertion Sort
-def insertionSort(arr):
-    # Traverse through 1 to len(arr)
-    for i in range(1, len(arr)):
-        j = i - 1
-        while j >= 0 and arr[j + 1] < arr[j]:
-            # arr[j] and arr[j + 1] are out of order so swap them
-            tmp = arr[j + 1]
-            arr[j + 1] = arr[j]
-            arr[j] = tmp
-            j -= 1
-    return arr
+# Define the unsorted array
+arr = [10, 5, 13, 8, 2]
+
+# Iterate through each element in the array
+for i in range(1, len(arr)):
+    # Save the current element
+    current = arr[i]
+    # Save the index of the element before the current element
+    j = i - 1
+    # Shift elements to the right until the correct position for the current element is found
+    while j >= 0 and arr[j] > current:
+        arr[j + 1] = arr[j]
+        j -= 1
+    # Insert the current element in its correct position
+    arr[j + 1] = current
+
+# Print the sorted array
+print(arr)
