f440dc4ae7049f09bcc4377ffa27b243e88fba77
TejasAnand <anandtejas455@gmail.com>
2023-08-04 16:45:27 +0530

    bfs

--- trees/bfs_implementation.py
+++ trees/bfs_implementation.py
@@ -1,20 +1,24 @@
 from collections import deque
 
+# we use queues for bfs because we need the FIFO functionality.
+# The FIFO concept that underlies a Queue will ensure that those things
+# that were discovered first will be explored first.
+
 
 def bfs(root):
     queue = deque()
 
     if root:
-        queue.append(root)
+        queue.append(root)  # first in
 
     level = 0
     while len(queue) > 0:
         print("level: ", level)
         for i in range(len(queue)):
-            curr = queue.popleft()
+            curr = queue.popleft()  # first out
             print(curr.val)
             if curr.left:
-                queue. append(curr. left)
+                queue. append(curr.left)
             if curr.right:
-                queue.append(curr. right)
+                queue.append(curr.right)
     level += 1
